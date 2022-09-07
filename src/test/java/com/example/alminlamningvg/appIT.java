a0ffe1f57c948d79c4874742044443f992389462
EmilWikell <91671600+EmilWikell@users.noreply.github.com>
2022-09-07 13:47:50 +0200

    added spring to project and a test for a restcontroller

---
+++ src/test/java/com/example/alminlamningvg/appIT.java
@@ -0,0 +1,28 @@
+package com.example.alminlamningvg;
+
+
+import io.restassured.RestAssured;
+import io.restassured.parsing.Parser;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.equalTo;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class appIT {
+
+
+    @BeforeAll
+    public static void beforeAll() {
+
+    }
+    @Test
+    public void testHelloWorld(){
+        RestAssured.registerParser("text/plain", Parser.HTML);
+        when().get().then()
+                .statusCode(200)
+                .body("html.body", equalTo("Hello World"));
+    }
+}
